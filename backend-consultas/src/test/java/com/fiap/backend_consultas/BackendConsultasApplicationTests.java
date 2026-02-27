e8506e6e0a56613a0a3e20f2065e69954d67cac8
Lucas Grecco <lucas.rgrecco@gmail.com>
2026-02-27 08:35:47 -0300

    estrutura inicial do projeto aula 27-02

---
+++ backend-consultas/src/test/java/com/fiap/backend_consultas/BackendConsultasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.backend_consultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendConsultasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
