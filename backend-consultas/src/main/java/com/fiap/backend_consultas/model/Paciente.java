d2fb2ef61164289b76430bd7b33db71296adc1db
Lucas Grecco <lucas.rgrecco@gmail.com>
2026-03-02 08:23:17 -0300

    feat : Implementando Atributos adicionais ao Paciente

--- backend-consultas/src/main/java/com/fiap/backend_consultas/model/Paciente.java
+++ backend-consultas/src/main/java/com/fiap/backend_consultas/model/Paciente.java
@@ -2,6 +2,8 @@ package com.fiap.backend_consultas.model;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "pacientes")
 public class Paciente {
@@ -10,6 +12,13 @@ public class Paciente {
     private Long id;
     @Column(nullable = false)
     private String nome;
+    @Column(nullable = false)
+    private String email;
+    private String telefone;
+    @Column(nullable = false)
+    private LocalDate dataNascimento;
+    @Column(nullable = false)
+    private Boolean ativo;
 
     public Long getId() {
         return id;
@@ -25,5 +34,37 @@ public class Paciente {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
     }
 }
